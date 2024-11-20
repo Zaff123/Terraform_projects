49bdf67bb8ae7bc0353dbcddfa831c114612f61a
Md Zaffaruddin <48831733+Zaff123@users.noreply.github.com>
2024-11-20 13:12:50 +0530

    Update main.tf

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 resource "aws_instance" "abhishek" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami = "ami-0dee22c13ea7a9a67" 
+  subnet_id = "subnet-019ea91ed9b5252e7"
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
