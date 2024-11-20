e5f110f07e700feeed7f12255d7bdf332eddd203
Md Zaffaruddin <48831733+Zaff123@users.noreply.github.com>
2024-11-20 14:16:32 +0530

    Update main.tf

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "zaff-TF" {
   instance_type = "t2.micro"
   ami = "ami-0dee22c13ea7a9a67" 
   subnet_id = "subnet-019ea91ed9b5252e7"
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "zaff-bkt" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
